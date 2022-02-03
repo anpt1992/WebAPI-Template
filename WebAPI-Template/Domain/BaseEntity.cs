2c753a38ef3ebd6938c563e05ed108ad3900e7f3
Pham Truong An <pham.an.20.09@gmail.com>
2022-02-03 17:04:17 +0700

    BaseEntity

---
+++ WebAPI-Template/Domain/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_Template.Domain
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime CreatedDate { get; set; }
+
+        [MaxLength(256)]
+        [ScaffoldColumn(false)]
+        public string? CreatedBy { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime UpdatedDate { get; set; }
+
+        [MaxLength(256)]
+        [ScaffoldColumn(false)]
+        public string? UpdatedBy { get; set; }
+
+        [ScaffoldColumn(false)]
+        public byte SoftDeleteLevel { get; set; }
+    }
+
+}
